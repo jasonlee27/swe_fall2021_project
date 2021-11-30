41aebc47c9982e3ce6fb5673cdff2f2d5b30e7f3
Jaeseong Lee <jason.lee27@utexas.edu>
2021-11-30 12:52:31 -0600

    debugging init db

--- backend/src/db_init.sql
+++ backend/src/db_init.sql
@@ -10,9 +10,9 @@ CREATE TABLE IF NOT EXISTS Accounts (
        email varchar(50) NOT NULL,
     PRIMARY KEY (id),
     UNIQUE(username)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+);
 
-INSERT INTO Accounts (id, username, password, email) VALUES (1, 'test', 'test@test.com');
+INSERT INTO Accounts (username, password, email) VALUES ('test_user', 'test_password', 'test@test.com') ON DUPLICATE KEY UPDATE id=id;
 
 
 -- Store Table
@@ -22,14 +22,14 @@ CREATE TABLE IF NOT EXISTS Stores (
   	loc_city varchar(50) NOT NULL,
   	loc_state varchar(2) NOT NULL,
     PRIMARY KEY (id)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+);
 
-INSERT INTO Stores (id, loc_city, loc_state) VALUES (1, '3566 hardman road', 'dallas', 'TX');
-INSERT INTO Stores (id, loc_city, loc_state) VALUES (2, '1234 coit rd', 'richardson', 'TX');
-INSERT INTO Stores (id, loc_city, loc_state) VALUES (3, '6789 frankford rd', 'richardson', 'TX');
-INSERT INTO Stores (id, loc_city, loc_state) VALUES (4, '3456 w campbell rd', 'richardson', 'TX');
-INSERT INTO Stores (id, loc_city, loc_state) VALUES (5, '1339 edgewood avenue', 'addison', 'TX');
-INSERT INTO Stores (id, loc_city, loc_state) VALUES (6, '1629 Orchard Street', 'plano', 'TX');
+INSERT INTO Stores (address, loc_city, loc_state) VALUES ('3566 hardman road', 'dallas', 'TX') ON DUPLICATE KEY UPDATE id=id;
+INSERT INTO Stores (address, loc_city, loc_state) VALUES ('1234 coit rd', 'richardson', 'TX') ON DUPLICATE KEY UPDATE id=id;
+INSERT INTO Stores (address, loc_city, loc_state) VALUES ('6789 frankford rd', 'richardson', 'TX') ON DUPLICATE KEY UPDATE id=id;
+INSERT INTO Stores (address, loc_city, loc_state) VALUES ('3456 w campbell rd', 'richardson', 'TX') ON DUPLICATE KEY UPDATE id=id;
+INSERT INTO Stores (address, loc_city, loc_state) VALUES ('1339 edgewood avenue', 'addison', 'TX') ON DUPLICATE KEY UPDATE id=id;
+INSERT INTO Stores (address, loc_city, loc_state) VALUES ('1629 Orchard Street', 'plano', 'TX') ON DUPLICATE KEY UPDATE id=id;
 
 
 -- Item Table
@@ -44,34 +44,34 @@ CREATE TABLE IF NOT EXISTS Items (
     PRIMARY KEY (id),
     UNIQUE (itemcode),
     FOREIGN KEY (store_id) REFERENCES Stores(id),
-    CONSTRAINT item_quantity_constraint CHECK (quantity>=0.0)
+    CONSTRAINT item_quantity_constraint CHECK (quantity>=0.0),
     CONSTRAINT item_price_constraint CHECK (price>=0.0)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+);
 
-INSERT INTO Items (id, store_id, itemname, itemcode, price, expirydate, quantity) VALUES (1, 3, 'apple', '000000000001', 0.8, '12012021', 300);
-INSERT INTO Items (id, store_id, itemname, itemcode, price, expirydate, quantity) VALUES (2, 3, 'eggs brown medium grade a', '000000000002', 4.29, '12012021', 200);
-INSERT INTO Items (id, itemname, itemcode, price, expirydate, quantity) VALUES (3, 'fat free greek yogurt', '000000000003', 5.49, '11222021', 200);
-INSERT INTO Items (id, store_id, itemname, itemcode, price, expirydate, quantity) VALUES (4, 3, 'broccoli florets', '000000000004', 2.59, '11222021', 200);
-INSERT INTO Items (id, store_id, itemname, itemcode, price, expirydate, quantity) VALUES (5, 4, 'peanut butter crunchy', '000000000005', 4.1, '11222022', 160);
-INSERT INTO Items (id, store_id, itemname, itemcode, price, expirydate, quantity) VALUES (6, 4, 'baby spinach salad', '000000000006', 2.29, '11222021', 200);
-INSERT INTO Items (id, store_id, itemname, itemcode, price, expirydate, quantity) VALUES (7, 1, 'water spring', '000000000007', 0.89, '11192021', 300);
-INSERT INTO Items (id, store_id, itemname, itemcode, price, expirydate, quantity) VALUES (8, 2, 'paper towels 6 pack', '000000000008', 6.49, '10102022', 400);
-INSERT INTO Items (id, store_id, itemname, itemcode, price, expirydate, quantity) VALUES (9, 5, 'paper towels 6 pack', '000000000008', 6.49, '10102022', 400);
-INSERT INTO Items (id, store_id, itemname, itemcode, price, expirydate, quantity) VALUES (10, 6, 'broccoli florets', '000000000004', 2.59, '11202021', 200);
+INSERT INTO Items (id, store_id, itemname, itemcode, price, expirydate, quantity) VALUES (1, 3, 'apple', '000000000001', 0.8, '12012021', 300) ON DUPLICATE KEY UPDATE id=id;
+INSERT INTO Items (id, store_id, itemname, itemcode, price, expirydate, quantity) VALUES (2, 3, 'eggs brown medium grade a', '000000000002', 4.29, '12012021', 200) ON DUPLICATE KEY UPDATE id=id;
+INSERT INTO Items (id, store_id, itemname, itemcode, price, expirydate, quantity) VALUES (3, 1, 'fat free greek yogurt', '000000000003', 5.49, '11222021', 200) ON DUPLICATE KEY UPDATE id=id;
+INSERT INTO Items (id, store_id, itemname, itemcode, price, expirydate, quantity) VALUES (4, 3, 'broccoli florets', '000000000004', 2.59, '11222021', 200) ON DUPLICATE KEY UPDATE id=id;
+INSERT INTO Items (id, store_id, itemname, itemcode, price, expirydate, quantity) VALUES (5, 4, 'peanut butter crunchy', '000000000005', 4.1, '11222022', 160) ON DUPLICATE KEY UPDATE id=id;
+INSERT INTO Items (id, store_id, itemname, itemcode, price, expirydate, quantity) VALUES (6, 4, 'baby spinach salad', '000000000006', 2.29, '11222021', 200) ON DUPLICATE KEY UPDATE id=id;
+INSERT INTO Items (id, store_id, itemname, itemcode, price, expirydate, quantity) VALUES (7, 1, 'water spring', '000000000007', 0.89, '11192021', 300) ON DUPLICATE KEY UPDATE id=id;
+INSERT INTO Items (id, store_id, itemname, itemcode, price, expirydate, quantity) VALUES (8, 2, 'paper towels 6 pack', '000000000008', 6.49, '10102022', 400) ON DUPLICATE KEY UPDATE id=id;
+INSERT INTO Items (id, store_id, itemname, itemcode, price, expirydate, quantity) VALUES (9, 5, 'paper towels 6 pack', '000000000009', 6.49, '10102022', 400) ON DUPLICATE KEY UPDATE id=id;
+INSERT INTO Items (id, store_id, itemname, itemcode, price, expirydate, quantity) VALUES (10, 6, 'broccoli florets', '000000000010', 2.59, '11202021', 200) ON DUPLICATE KEY UPDATE id=id;
 
 
 -- Order Table
 CREATE TABLE IF NOT EXISTS Orders (
 	id int(11) NOT NULL AUTO_INCREMENT,
   	order_date varchar(50) NOT NULL,
-        username  varchar(20) NOT NULL,
+        username  varchar(50) NOT NULL,
   	items varchar(20) NOT NULL,
         order_type varchar(20) NOT NULL,
         price float(8,3) NOT NULL,
-        address varchar(50)
+        address varchar(50),
         shipping_method varchar(20),
     PRIMARY KEY (id),
     FOREIGN KEY (username) REFERENCES Accounts(username),
     CONSTRAINT order_type_constraint CHECK (order_type = 'pickup' OR order_type = 'delivery'),
-    CONSTRAINT price_constraint CHECK (price>=0.0),
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+    CONSTRAINT price_constraint CHECK (price>=0.0)
+);
